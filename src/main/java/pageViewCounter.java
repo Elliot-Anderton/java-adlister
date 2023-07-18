60ca7cae4c6681d05bb6804f5e34b6d9397f72d9
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-07-18 10:19:24 -0700

    Finished exercises for servlets

---
+++ src/main/java/pageViewCounter.java
@@ -0,0 +1,31 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "pageViewCounter", urlPatterns = "/count")
+public class pageViewCounter extends HttpServlet {
+    public static int count = 1;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String reset = req.getParameter("reset");
+        try {
+            if (reset.equals("y")) {
+                count = 1;
+                out.println(String.format("Views on this page: %d", count));
+                count++;
+            } else {
+                out.println(String.format("Views on this page: %d", count));
+                count++;
+            }
+        } catch (Exception e) {
+            out.println(String.format("Views on this page: %d", count));
+            count++;
+        }
+    }
+}
